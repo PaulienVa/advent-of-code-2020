cd994376a66ae3dc2ce785554fcce0a22a8cd7da
Paulien van Alst <coding@paulienvanalst.com>
2020-12-09 18:16:31 +0100

    Day 4: refactoring not leading to a different outcome

--- src/main/kotlin/nl/paulienvanalst/adventOfCode/twentytwenty/Dec4/Dec04.kts
+++ src/main/kotlin/nl/paulienvanalst/adventOfCode/twentytwenty/Dec4/Dec04.kts
@@ -4,95 +4,87 @@ package nl.paulienvanalst.adventOfCode.twentytwenty.Dec4
 
 import nl.paulienvanalst.adventOfCode.twentytwenty.utils.Utils
 
-val input = Utils.readFileAsWhole()
-val passports = input.split("\n\n")
+val passports = Utils.readFileAsWhole().split("\n\n").map { it.replace("\n", " ") }
 
-val byr = "byr"
-val iyr = "iyr"
-val eyr = "eyr"
-val hgt = "hgt"
-val hcl = "hcl"
-val ecl = "ecl"
-val pid = "pid"
+val mandatoryFields = listOf(
+    "byr:",
+    "iyr:",
+    "eyr:",
+    "hgt:",
+    "hcl:",
+    "ecl:",
+    "pid:"
+)
 
-fun containsMandatoryFields(it: String) =
-    (it.contains(byr) && it.contains(iyr) && it.contains(eyr)
-            && it.contains(hgt) && it.contains(hcl) && it.contains(ecl)
-            && it.contains(pid))
-
-val minimalValidPassports = passports.filter {
-    containsMandatoryFields(it)
+val minimalValidPassports = passports.filter { passport ->
+    mandatoryFields.all { passport.contains(it) }
 }
 
 println("Solution to part 1:")
 
 println("Found ${minimalValidPassports.size} (182) valid passports in total ${passports.size}")
 
-
 println("Solution to part 2:")
 
-val validPassports = minimalValidPassports.map { it to validPassport(it) }.filter{ it.second }
+val validPassports = minimalValidPassports.map { it to validPassport(it) }.filter { it.second }
 
-//validPassports.forEach { println("Passport: ${it.first} and valid ${it.second} \n") }
+println("Found ${validPassports.size} (110, not the correct one) valid passports in total ${passports.size}")
 
-println("Found ${validPassports.size} (28) valid passports in total ${passports.size}")
+fun validPassport(passport: String) = validations().all { it(passport) }
 
+fun validations() : List<(String) -> Boolean> = listOf(
+    {passport -> validBirthYear(passport)},
+    {passport -> validIssueYear(passport)},
+    {passport -> validExpirationYear(passport)},
+    {passport -> heightFieldValidation(passport)},
+    {passport -> hairlengthValidation(passport)},
+    {passport -> eyeColourValidation(passport)},
+    {passport -> passportIdValidation(passport)}
+)
 
-fun validPassport(passport: String) : Boolean = validBirthYear(passport) && validIssueYear(passport)
-        && validExpirationYear(passport) && heightFieldValidation(passport) && hairlengthValidation(passport)
-        && eyeColourValidation(passport) && passportIdValidation(passport)
-
-
-fun validBirthYear(passport: String) = yearFieldValidation(passport, byr, 1920, 2002)
-fun validIssueYear(passport: String) = yearFieldValidation(passport, iyr, 2010, 2020)
-fun validExpirationYear(passport: String) = yearFieldValidation(passport, eyr, 2020, 2030)
+fun validBirthYear(passport: String) = yearFieldValidation(passport, "byr", 1920, 2002)
+fun validIssueYear(passport: String) = yearFieldValidation(passport, "iyr", 2010, 2020)
+fun validExpirationYear(passport: String) = yearFieldValidation(passport, "eyr", 2020, 2030)
 
 fun yearFieldValidation(passport: String, field: String, lowerBoundary: Int, higherBoundary: Int): Boolean {
     val regex = "(?<=$field:)(\\d{4})".toRegex()
     val year = regex.find(passport)?.groupValues?.get(0)
 
-    return year != null && year.toInt() in lowerBoundary .. higherBoundary
+    return year != null && year.toInt() in lowerBoundary..higherBoundary
 }
 
 fun heightFieldValidation(passport: String): Boolean {
-    val regex = "(?<=$hgt:)(\\d*)(?=cm|in)".toRegex()
+    val regex = "(?<=hgt:)(\\d*)(?=cm|in)".toRegex()
     val height = regex.find(passport)?.groupValues?.get(0)
-    val unitRegex = "(?<=$hgt:\\d*)(cm|in)".toRegex()
+    val unitRegex = "(?<=hgt:\\d*)(cm|in)".toRegex()
     val unit = unitRegex.find(passport)?.groupValues?.get(0)
 
     if (height == null || unit == null) {
         return false
     }
 
-    val b = (unit == "cm" && height.toInt() in 150..193
+    return (unit == "cm" && height.toInt() in 150..193
             || unit == "in" && height.toInt() in 59..76)
-//    println("Validating height $height in unit $unit and valid $b")
-    return b
 }
 
 fun hairlengthValidation(passport: String): Boolean {
-    val regex = "(?<=$hcl:)(#[0-9a-f]{6})".toRegex()
+    val regex = "(?<=hcl:)(\\#[0-9a-f]{6})".toRegex()
     val hairLength = regex.find(passport)?.groupValues?.get(0)
 
-    val b = hairLength != null
-//    println("valid hairlength: $hairLength and $b")
-    return b
+    return hairLength != null
 }
 
-fun eyeColourValidation(passport: String) : Boolean {
-    val regex = "(?<=$ecl:)([a-z]{3})".toRegex()
+fun eyeColourValidation(passport: String): Boolean {
+    val regex = "(?<=ecl:)([a-z]{3})".toRegex()
     val eyeColour = regex.find(passport)?.groupValues?.get(0)
     val validColours = listOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth")
-    val b = eyeColour != null && eyeColour in validColours
 
-//    println("valid eyeColour: $eyeColour and valid $b")
-    return b
+    return eyeColour != null && eyeColour in validColours
 }
 
-fun passportIdValidation(passport: String) : Boolean {
-    val regex = "(?<=($pid:))(\\d{9})".toRegex()
+fun passportIdValidation(passport: String): Boolean {
+    val regex = "(?<=(pid:))(\\d{9})".toRegex()
     val passportId = regex.find(passport)?.groupValues?.get(0)
 
-//    println("Validating passportId: $passportId")
     return passportId != null
 }
